69a93546c59739b60c1ed9a5a80d1cdfd390cdcb
Pooya Golchian <pooya.golchian@gmail.com>
2024-06-10 16:01:09 +0400

    add refresh token

--- src/iam/authentication/authentication.controller.ts
+++ src/iam/authentication/authentication.controller.ts
@@ -4,6 +4,7 @@ import { SignUpDto } from './dto/sign-up.dto/sign-up.dto';
 import { SignInDto } from './dto/sign-in.dto/sign-in.dto';
 import { Auth } from '../decorators/auth.decorator';
 import { AuthType } from '../enums/auth-type.enum';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
 
 @Auth(AuthType.None)
 @Controller('authentication')
@@ -15,9 +16,9 @@ export class AuthenticationController {
     return this.authService.signUp(signUpDto);
   }
 
-  @HttpCode(HttpStatus.OK)
-  @Post('sign-in')
-  asyncsignIn(@Body() signInDto: SignInDto) {
-    return this.authService.signIn(signInDto);
+  @HttpCode(HttpStatus.OK) // changed since the default is 201
+  @Post('refresh-tokens')
+  refreshTokens(@Body() refreshTokenDto: RefreshTokenDto) {
+    return this.authService.refreshTokens(refreshTokenDto);
   }
 }
