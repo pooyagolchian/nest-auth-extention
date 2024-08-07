9451c7d2e5b197c40fd39ba310988e7d1962316e
Pooya Golchian <pooya.golchian@gmail.com>
2024-08-07 17:27:00 +0400

    remove console

--- src/iam/authentication/social/google-authentication.service.ts
+++ src/iam/authentication/social/google-authentication.service.ts
@@ -25,7 +25,6 @@ export class GoogleAuthenticationService implements OnModuleInit {
         const clientId = this.configService.get('GOOGLE_CLIENT_ID');
         const clientSecret = this.configService.get('GOOGLE_CLIENT_SECRET');
         this.oauthClient = new OAuth2Client(clientId, clientSecret);
-
     }
 
     async authenticate(token: string) {
